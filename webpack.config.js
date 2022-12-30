6843dcf63f9859caed0741ad61a85611e742079c
Haofeng Feng <fenghaofeng@live.com>
2022-12-29 18:23:57 -0800

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist/assets')
+    path: path.resolve(__dirname, 'public/assets')
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
